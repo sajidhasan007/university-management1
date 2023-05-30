a03d20929a34e33796322951baf3d411e08b2b68
sajidhasan007 <sajid.hasan@misfit.tech>
2023-05-30 11:22:07 +0600

    add lintin and pritter

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,11 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
-const app: Application = express();
-const port = 3000;
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded());
-app.get("/", (req: Request, res: Response) => {
-  res.send("Working...");
-});
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
+app.get('/', (req: Request, res: Response) => {
+  res.send('Working...')
+})
 
-export default app;
+export default app
