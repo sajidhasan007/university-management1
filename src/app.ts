27a68559f1da57ab600d64f069e7a95710ad0a15
sajidhasan007 <sajid.hasan.cit@gmail.com>
2023-05-30 08:50:22 +0600

    project set up

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+const app: Application = express();
+const port = 3000;
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+app.get("/", (req: Request, res: Response) => {
+  res.send("Working...");
+});
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config?.port}`);
+    });
+  } catch (error) {
+    console.log("failed to connect db", error);
+  }
+}
+main();
