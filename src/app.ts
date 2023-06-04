a4a43b42bb462bb6f24b9eaf79edd458f4a30499
sajidhasan007 <sajid.hasan@misfit.tech>
2023-06-04 10:14:21 +0600

    add user controller and router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,21 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+import usersRouter from './app/modules/users/user.router'
 const app: Application = express()
+
 app.use(cors())
+
+//parser
 app.use(express.json())
-app.use(express.urlencoded())
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working....')
+app.use(express.urlencoded({ extended: true }))
+
+// Application routes
+
+app.use('/api/v1/users/', usersRouter)
+
+//Testing
+app.get('/', async (req: Request, res: Response) => {
+  res.send('Working Successfully')
 })
 
 export default app
--- src/app/modules/users/user.service.ts
+++ src/app/modules/users/user.service.ts
@@ -1,12 +1,22 @@
 import { IUser } from './user.interface'
 import { User } from './user.model'
+import { generateUserId } from './user.utils'
 
-const createUser = (user: IUser): Promise<IUser | null> => {
-  const createUser = User.create(user)
-  if (!createUser) {
-    throw new Error('failed to create user')
+const createUser = async (user: IUser): Promise<IUser | null> => {
+  // auto generated incremental id
+  const id = await generateUserId()
+  user.id = id
+  // default password
+  if (!user.password) {
+    user.password = 'Pass123#'
   }
-  return createUser
+
+  const createdUser = await User.create(user)
+
+  if (!createUser) {
+    throw new Error('Failed to create user!')
+  }
+  return createdUser
 }
 
 export default {
