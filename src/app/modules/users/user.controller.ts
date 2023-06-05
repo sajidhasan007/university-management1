4872a7528e9613ab23aa776f8d60bd7ecdd591d7
sajidhasan007 <sajid.hasan@misfit.tech>
2023-06-05 20:12:13 +0600

    add more error handler

--- src/app/modules/users/user.controller.ts
+++ src/app/modules/users/user.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
 import { UserService } from './user.service'
+
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { user } = req.body
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,42 @@
+/* eslint-disable no-undef */
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
 import { errorlogger, logger } from './shared/looger'
+import { Server } from 'http'
+
+process.on('uncaughtException', error => {
+  errorlogger.error(error)
+  process.exit(1)
+})
+
+let server: Server
 
 async function main() {
   try {
     await mongoose.connect(config.databaseUrl as string)
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`Application  listening on port ${config.port}`)
     })
   } catch (error) {
     errorlogger.error('Failed to connect database', error)
   }
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 main()
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received')
+  if (server) {
+    server.close()
+  }
+})
