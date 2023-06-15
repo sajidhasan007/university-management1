42e1a81f1251ea8774cf52a2deae1f621fdce8b2
sajidhasan007 <sajid.hasan@misfit.tech>
2023-06-15 09:29:41 +0600

    change in user module

--- src/app/modules/users/user.interface.ts
+++ src/app/modules/users/user.interface.ts
@@ -1,9 +1,10 @@
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 
 export type IUser = {
   id: string;
   role: string;
   password: string;
+  student?: Types.ObjectId | IStudent;
 };
 
-export type UserModel = Model<IUser, object>;
+export type UserModel = Model<IUser, Record<string, unknown>>;
