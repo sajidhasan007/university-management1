911cbb1b45f519e36529a3f493cd89e73a855b3f
sajidhasan007 <sajid.hasan@misfit.tech>
2023-06-22 12:09:06 +0600

    auth added

--- src/config/index.ts
+++ src/config/index.ts
@@ -9,4 +9,10 @@ export default {
   port: process.env.PORT,
   databaseUrl: process.env.DATABASE_URL,
   saltRounds: process.env.SALT_ROUNDS,
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    refresh_secret: process.env.JWT_REFRESH_SECRET,
+    expires_in: process.env.JWT_EXPIRES_IN,
+    refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  },
 };
